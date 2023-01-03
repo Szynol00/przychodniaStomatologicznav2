e05c1337f58a6ef631723e104e924e12a0323f9d
Szymon <calka.szymek@gmail.com>
2023-01-03 16:10:48 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.przychodniastomatologicznav2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.przychodniastomatologicznav2 to javafx.fxml;
+    exports com.example.przychodniastomatologicznav2;
+}
